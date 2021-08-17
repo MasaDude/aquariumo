b83525f6c0e92ed473a7b2ef06e9795e774dd553
MasaDude <87702357+MasaDude@users.noreply.github.com>
2021-08-17 14:59:01 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+$(".nav a").on("click", function(event) {
+    if(this.hash !== "") {
+        event.preventDefault();
+        const hash = this.hash;
+        $("html, body").animate({scrollTop: $(hash).offset().top - 0}, 800);
+
+    }
+
+});
+
+
+window.addEventListener("scroll", function() {
+    if(window.scrollY > 150) {
+        document.querySelector(".nav").style.opacity = 0.9;
+
+    } else {
+        document.querySelector(".nav").style.opacity = 1;
+    }
+});
